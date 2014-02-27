9f45d289e8494305ad0fb0618672c882ca5a662a
Derek Andre <dcandre@yahoo.com>
2014-02-26 22:57:01 -0600

    Updated main.js

--- cal/js/main.js
+++ cal/js/main.js
@@ -2,31 +2,44 @@
 
 $(function(){
     
+    //For the purpose of this demo I will override Backbone.sync to simulate connecting to a web server
+    Backbone.sync = function(method, model, options) 
+    {
+        var url = model.url();
+        
+        if(method == "read")
+        {
+            if(_.isArray(url.match(/\/test\/\d+\//i)))
+            {
+                //This is a Test Model
+                model.parse({id: 1, title: "Test Title", description: "Thank you for taking my test.  Please click the 'Start' button.", numberOfQuestions: 1});
+            }
+            else if(_.isArray(url.match(/\/test\/\d+\/\/question\/\d+\//i)))
+            {
+                //This is a Question Model
+                model.parse({id: 1, text: "This is question 1", testID: 1, index: 0, answers: []});
+            }
+        }
+
+        options.success(model);
+    };
+    
     //Models Namespace
     dcandre.Models = {};
 
-    //Answer Model
+    //Router Model
+    dcandre.Models.Router = Backbone.Router.extend({
+        routes: {
+            "test/:id/": "getTestByID",
+            "test/:id/question/:index/": "getQuestionByIndex",
+            "*actions": "default"
+        }
+    });
+
+    //Abstract Answer Model
     dcandre.Models.Answer = Backbone.Model.extend({
-        constructor: function() 
-        {
-            Backbone.Model.apply(this, arguments);
-
-            var id = this.get("id");
-
-            if("undefined" == typeof id || null == id || id <= 0)
-            {
-                alert("An id attribute, that is great than 0, is required.");
-            }
-
-            var text = this.get("text");
-
-            if("undefined" == typeof text || null == text)
-            {
-                alert("An answer needs text.");
-            }
-        },
         defaults: {
-            isCorrectAnswer: false
+            "id": 0
         }
     });
 
@@ -35,59 +48,103 @@ $(function(){
         model: dcandre.Models.Answer
     });
     
-    //Question Model
-    dcandre.Models.Question = Backbone.Model.extend({
-        constructor: function() 
+    //Abstract Question Model
+    dcandre.Models.Question = Backbone.Model.extend({});
+
+    //Multiple Choice Question Model
+    dcandre.Models.MultipleChoiceQuestion = dcandre.Models.Question.extend({
+        url: function()
         {
-            Backbone.Model.apply(this, arguments);
-
-            var id = this.get("id");
-            
-            if("undefined" == typeof id || null == id || id <= 0)
-            {
-                alert("An id attribute, that is great than 0, is required.");
-            }
-
-            var text = this.get("text");
-
-            if("undefined" == typeof text || null == text)
-            {
-                alert("A question needs text.");
-            }            
+             return "/test/" + this.get("testID") + "/question/index/" + this.get("index");    
         },
         defaults: {
-            "answers": new dcandre.Models.Answers(),
-            "correctAnswerIDs" : new Array()
-        }
-    });
+            "id": 0,
+            "index": -1,
+            "testID": 0,
+            "text": "",
+            "answers": new dcandre.Models.Answers
+        },
+        parse: function(response, options)
+        {
 
-    //Question Collection Model
-    dcandre.Models.Questions = Backbone.Collection.extend({
-        model: dcandre.Models.Question
+        }
     });
 
     //Test Model
     dcandre.Models.Test = Backbone.Model.extend({
-        constructor: function() 
+        url: function()
         {
-            Backbone.Model.apply(this, arguments);
-        },
+            return "/test/" + this.get("id") + "/";
+        },        
         defaults: {
-            "questions": new dcandre.Models.Questions()
+            "id": 0,
+            "title": null,
+            "description": null,
+            "numberOfQuestions": 0
+        },
+        parse: function(response, options)
+        {
+            if(_.has(response, "id"))
+            {
+                this.set("id", response.id);
+            }
+
+            if(_.has(response, "title"))
+            {
+                this.set("title", response.title);
+            }
+
+            if(_.has(response, "description"))
+            {
+                this.set("description", response.description);
+            }
+
+            if(_.has(response, "numberOfQuestions"))
+            {
+                this.set("numberOfQuestions", response.numberOfQuestions);
+            }
+
+            return this;
         }
     });
 
-    dcandre.test = new dcandre.Models.Test();
-    var questions = dcandre.test.get("questions");
 
-    var question1 = new dcandre.Models.Question({ id: 1, text: "Question 1"});
-    var questionAnswer1 = new dcandre.Models.Answer({ id: 1, text: "Answer 1"});
-    var questionAnswer2 = new dcandre.Models.Answer({ id: 2, text: "Answer 2", isCorrectAnswer: true});
-    var questionAnswer3 = new dcandre.Models.Answer({ id: 3, text: "Answer 3"});
-    var questionAnswer4 = new dcandre.Models.Answer({ id: 4, text: "Answer 4"});
-    
-    question1.get("answers").add([questionAnswer1, questionAnswer2, questionAnswer3, questionAnswer4]);
+    //Main Namespace
+    dcandre.Main = {};
 
-    questions.add([question1]);
+    //Main Router
+    dcandre.Main.Router = new dcandre.Models.Router();
     
+    //Main Test
+    dcandre.Main.Test = new dcandre.Models.Test();
+    
+    //Main Question
+    dcandre.Main.Question = new dcandre.Models.MultipleChoiceQuestion();
+    
+    //Get Test By ID route
+    dcandre.Main.Router.on("route:getTestByID", function(id)
+    {        
+        if(null != id && id > 0)
+        {
+            dcandre.Main.Test.set("id", id);
+            dcandre.Main.Test.fetch();
+        }
+    });
+
+    //Get Question By Index
+    dcandre.Main.Router.on("route:getQuestionByIndex", function(testID, index)
+    {        
+        if(null != testID && testID > 0 && null != index && index > 0)
+        {
+            
+        }
+    });
+
+    //The default route event which will catch everyting else
+    dcandre.Main.Router.on("route:default", function(actions)
+    {
+        this.navigate("test/1/");
+    });
+
+    Backbone.history.start();
 });
