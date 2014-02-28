1f13b3869261bd79860299f37fd0e68fecc66fbc
Derek Andre <dcandre@yahoo.com>
2014-02-28 01:08:11 -0600

    Need to finish views

--- cal/js/main.js
+++ cal/js/main.js
@@ -20,12 +20,12 @@ $(function(){
                 var numberOfCorrectAnswers = (dcandre.Main.UserAnswer.isCorrect) ? 1 : 0;
 
                 //This is a TestResult Model
-                model.parse({id: 1, testID: 1, numberOfCorrectAnswers: 0, numberofQuestions: 1});
+                model.parse({id: 1, testID: 1, userID: 1, numberOfCorrectAnswers: 0, numberofQuestions: 1});
             }
             else if(_.isArray(url.match(/\/test\/\d+\//i)))
-            {               
+            {              
                 //This is a Test Model
-                model.parse({id: 1, title: "Test Title", description: "Thank you for taking my test.  Please click the 'Start' button.", numberOfQuestions: 1});
+                model.parse({id: 1, title: "Derek's Test", description: "Thank you for taking my test.  Please click the 'Start' button.", numberOfQuestions: 1});
             }            
         }
         else if(method == "update")
@@ -39,7 +39,9 @@ $(function(){
                 //This is a UserAnswer Model
                 model.parse({id: 1, userID: 1, answerIDs: correctAnswerIDs, isCorrect: isAnswerCorrect});
             }
-        }        
+        } 
+        
+        options.success(model);
     };
     
     //Models Namespace
@@ -233,6 +235,7 @@ $(function(){
         defaults: {
             "id": 0,
             "testID": 0,
+            "userID": 0,
             "numberOfCorrectAnswers": 0,
             "numberOfQuestions": 0
         },
@@ -247,9 +250,109 @@ $(function(){
         }
     });
 
+
+    //View Namespace
+    dcandre.Views = {};
+
+    //Multiple Choice Test View
+    dcandre.Views.MultipleChoiceTestView = Backbone.View.extend({
+        initialize: function()
+        {
+            this.$el = $(this.el);
+
+            this.model.on("sync", function()
+            {
+                this.render();
+            }, this);
+        },
+        render: function()
+        {
+            var template = _.template( $("#MultipleChoiceTestViewTemplate").html(), {test_title: this.model.get("title"), test_description: this.model.get("description")});
+            this.$el.html(template);
+                        
+            this.$el.removeClass().addClass("hide multipleChoiceTest").addClass("show", 500, function(){$(this).removeClass("hide")});
+            
+            $("#testArea input[type=submit]").button();
+           
+            this.delegateEvents({
+                "click input[value=Start]": "onStartButtonClick"    
+            });
+        }, 
+        onStartButtonClick: function(event)
+        {
+            self.trigger("start:test:request", {model: this.model});           
+        }
+    });
+
+    //Multiple Choice Question View
+    dcandre.Views.MultipleChoiceQuestionView = Backbone.View.extend({
+        initialize: function()
+        {
+            this.$el = $(this.el);
+
+            this.model.on("sync", function()
+            {
+                this.render();
+            }, this);
+        },
+        render: function()
+        {
+            var variables = {question_text: this.model.get("text"), answers: "<ul>"};
+            
+            this.model.get("answers").forEach(function(answer, index, list)
+            {
+                var answerID = answer.get("id");
+                var answerElementID = "answer" + answerID; 
+                this.answers += "<li><input type='checkbox' id='"+answerElementID+"' data-answer-id='"+answerID+"'><label for='"+answerElementID+"'>" + (index + 1) + "</label>"+answer.get("text")+"</li>";
+
+            }, variables);
+
+            variables.answers += "</ul>";
+
+            var template = _.template( $("#MultipleChoiceQuestionViewTemplate").html(), variables);
+            this.$el.html(template);
+
+            this.$el.removeClass().addClass("hide multipleChoiceQuestion").addClass("show", 500, function(){$(this).removeClass("hide")});
+
+            $("#testArea input[type=checkbox]").button();            
+            $("#testArea ul").buttonset();
+            $("#testArea input[type=submit]").button();
+            
+            this.delegateEvents({
+                "click input[type=submit]": "onSubmitButtonClick"
+            });            
+        },
+        onSubmitButtonClick: function(event)
+        {
+            var checkedAnswers = $("input:checked");
+            
+            if(checkedAnswers.length > 0)
+            {
+                var selectedAnswerIDs = new Array();
+
+                checkedAnswers.each(function(index, element) 
+                {
+                    var answerID = $(element).attr("data-answer-id");
+                    selectedAnswerIDs.push(answerID);
+                });
+
+                self.trigger("answer:submitted:from:question", {model: this.model, selectedAnswerIDs: selectedAnswerIDs});
+            }
+            else
+            {
+                //The user needs to select an answer
+            }
+        }
+    });
+
+
+
     //Main Namespace
     dcandre.Main = {};
     
+    
+    //Main Element ID
+    dcandre.Main.DisplayAreaID = "testArea";
 
     //User, or the person who is taking the test
     dcandre.Main.User = new dcandre.Models.User({id: 1, name: "Derek Andre", isAuthenticated: true});
@@ -269,69 +372,41 @@ $(function(){
     //Main Test Results
     dcandre.Main.TestResults = new dcandre.Models.TestResult();
 
+    //Main Test View
+    dcandre.Main.TestView = new dcandre.Views.MultipleChoiceTestView({model: dcandre.Main.Test, el: document.getElementById(dcandre.Main.DisplayAreaID)});
 
-    //Fetch The Main Test By ID
-    dcandre.Main.getTestByID = function(id)
-    {
+    //Main Question View
+    dcandre.Main.QuestionView = new dcandre.Views.MultipleChoiceQuestionView({model: dcandre.Main.Question, el: document.getElementById(dcandre.Main.DisplayAreaID)});
+
+    
+    //Listen for a test that 
+
+    //Get Test By ID route
+    dcandre.Main.Router.on("route:getTestByID", function(id)
+    {  
         if(null != id && id > 0)
         {
             dcandre.Main.Test.set("id", id);
             dcandre.Main.Test.fetch();
         }
-    };
-
-    //Fetch The Question By Index
-    dcandre.Main.getQuestionByIndex = function(testID, index)
-    {
-        if(null != testID && testID > 0 && null != index && index >= 0)
-        {   
-            dcandre.Main.Question.set("testID", testID);
-            dcandre.Main.Question.set("index", index);
-            dcandre.Main.Question.fetch();            
-        }
-    };
-
-    //Fetch The TestResults
-    dcandre.Main.getTestResultsByTestID = function(testID)
-    {
-        if(null != testID && testID > 0)
-        { 
-            dcandre.Main.TestResults.set("testID", testID);
-            dcandre.Main.TestResults.fetch();
-        }
-    };
-
-
-
-    //Get Test By ID route
-    dcandre.Main.Router.on("route:getTestByID", function(id)
-    {  
-        dcandre.Main.getTestByID(id);
     });
 
     //Get Question By Index
     dcandre.Main.Router.on("route:getQuestionByIndex", function(testID, index)
     {               
-        if(dcandre.Main.Test.get("id") <= 0)
+        if(index < dcandre.Main.Test.get("numberOfQuestions"))
         {
-            dcandre.Main.Test.once("change:numberOfQuestions", function(model)
-            {     
-                if(index < model.get("numberOfQuestions"))
-                {
-                    dcandre.Main.getQuestionByIndex(model.get("id"), index);  
-                }
-                else
-                {
-                    this.navigate("test/"+testID+"/results/", {trigger: true});
-                }
-            });
-
-            dcandre.Main.getTestByID(testID);
-        }  
-        else if(index < dcandre.Main.Test.get("numberOfQuestions"))
+            if(null != testID && testID > 0 && null != index && index >= 0)
+            {   
+                dcandre.Main.Question.set("testID", testID);
+                dcandre.Main.Question.set("index", index);
+                dcandre.Main.Question.fetch();            
+            }
+        }
+        else if(dcandre.Main.Test.get("id") <= 0)
         {
-            dcandre.Main.getQuestionByIndex(testID, index);  
-        }              
+            this.navigate("test/"+testID+"/", {trigger: true});
+        }                     
         else
         {
             this.navigate("test/"+testID+"/results/", {trigger: true});
@@ -340,9 +415,18 @@ $(function(){
 
     dcandre.Main.Router.on("route:getTestResultsByTestID", function(testID)
     {
-        dcandre.Main.getTestResultsByTestID(testID);
-
-        //TODO make sure no on comes in on this url
+        if(dcandre.Main.Test.get("id") >= 0)
+        {
+            if(null != testID && testID > 0)
+            { 
+                dcandre.Main.TestResults.set("testID", testID);
+                dcandre.Main.TestResults.fetch();
+            }
+        }
+        else
+        {
+            this.navigate("test/"+testID+"/", {trigger: true});
+        }
     });
 
     //The default route event which will catch everyting else
